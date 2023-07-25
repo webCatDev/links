177a5852950690bb7afbb873b4aa45692f36f6c6
webcatdev <webcatdev@gmail.com>
2023-07-25 17:09:39 +0300

    Add: Instagram icon

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 "use client";
 
-import Image from 'next/image';
-import Card from './Card';
-import GithubIcon from './Icons/GithubIcon';
+import Image from "next/image";
+import Card from "./Card";
+import GithubIcon from "./Icons/GithubIcon";
+import InstagramIcon from "./Icons/InstagramIcon";
 
 export default function Home() {
-  const handleMouseMove = e => {
+  const handleMouseMove = (e) => {
     for (const card of document.getElementsByClassName("card")) {
       const rect = card.getBoundingClientRect(),
         x = e.clientX - rect.left,
@@ -13,30 +14,38 @@ export default function Home() {
 
       card.style.setProperty("--mouse-x", `${x}px`);
       card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+    }
+  };
 
   return (
     <>
-
-      <div className='avatar'>
-        <Image src="/images/profile.jpg" width={100} height={100} alt='Hamza ve kedisi Böcüş' />
+      <div className="avatar">
+        <Image
+          src="/images/profile.jpg"
+          width={100}
+          height={100}
+          alt="Hamza ve kedisi Böcüş"
+        />
       </div>
 
-      <p className='description' >🚧 Yapım aşmasında</p>
+      <p className="description">🚧 Yapım aşamasındadır.</p>
 
       <div id="cards" onMouseMove={handleMouseMove}>
-
-        <Card href="https://github.com/webCatDev" icon={<GithubIcon />} title="webcatdev" />
-        <Card href="https://www.instagram.com/webcatdev/" icon={<GithubIcon />} title="@webcatdev" />
+        <Card
+          href="https://github.com/webCatDev"
+          icon={<GithubIcon />}
+          title="webcatdev"
+        />
+        <Card
+          href="https://www.instagram.com/webcatdev/"
+          icon={<InstagramIcon />}
+          title="@webcatdev"
+        />
         <Card href="#" icon="" title="Hello World" />
         <Card href="#" icon="" title="Hello World" />
         <Card href="#" icon="" title="Hello World" />
         <Card href="#" icon="" title="Hello World" />
-
-
       </div>
-
     </>
-  )
+  );
 }
